5a9b2e37f907420fcd20b9e74b1e62e33e732d24
running <running@choronzon.sway.arachne.cz>
2011-01-06 15:38:20 +0100

    some stuff

--- Okobot/Jerobot.pm
+++ Okobot/Jerobot.pm
@@ -7,6 +7,8 @@ use Data::Dumper;
 use warnings;
 use strict;
 
+use Algorithm::NaiveBayes;
+
 sub get_all {
 	my @arts;
 	my @pre_arts;
@@ -55,13 +57,63 @@ sub clean_short {
 		@s = @$VAR1;
 	}
 	
-	@s = map {$_->{body}=~s/<[^<]*>//g; $_} @s;
-	@s = grep {splitsize($_->{body})>=10} @s;
+	@s = map {$_->{body}=~s/<[^<]*>/ /g; $_} @s;
+	@s = grep {splitsize($_->{body})>=15} @s;
 	{
 		open my $f, ">", "jerome.out2";
 		print $f Dumper(\@s);
 		close $f;
 	}
+}
+
+sub bayes {
+	my $question = shift;
+	my $s;
+	$|=1;
+	
+	say "nacitani ...";
+	{
+		open my $f, "<", "jerome.out2";
+		say "pred joinem";
+		my $dumped = join ("", <$f>);
+		my $VAR1;
+		say "pred eval";
+		eval($dumped);
+		say "za eval";
+		close $f;
+		$s = $VAR1;
+	}
+	
+	my $nb = Algorithm::NaiveBayes->new;
+	
+	my %back;
+	
+	
+		say "pred add instance";
+	for my $a (@$s) {
+		my @words = split(/\ \.\?,!/, $a->{body});
+		my %counts;
+		for (@words) {$counts{$_}++};
+		$nb->add_instance (attributes => \%counts, label => $a->{id});
+		
+		$back{$a->{id}}=$a;
+	}
+	
+	say "begin train";
+	$nb->train;
+	say "end train";
+	
+	my @artwords = split(/\ \.\?,!/, $question);
+	my %artcounts;
+	for (@artwords) {$artcounts{$_}++};
+	
+	my $result = $nb->predict
+	    (attributes => {bar => 3, blurp => 2});
+	
+	my $best = (sort {$result->{$b} <=> $result->{$a}} keys %$result)[1];
+	
+	say $result->{$best};
+	say $back{$best}->{body};
 }
 
 1;
--- spust.pl
+++ spust.pl
@@ -3,5 +3,11 @@ use strict;
 use utf8;
 use 5.010;
 use Okobot::Jerobot;
+#use encoding 'utf8';
+binmode STDOUT, ":utf8";
+binmode STDIN, ":utf8";
 
-Okobot::Jerobot::clean_short;
+my $r = <>;
+say $r;
+
+Okobot::Jerobot::bayes($r);
